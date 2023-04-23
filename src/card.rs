571d7fc5d237ea9da43e710ad833b5be11e6096b
crai0n <52631259+crai0n@users.noreply.github.com>
2023-04-23 10:57:03 +0200

    added implementation for card from str

--- src/card.rs
+++ src/card.rs
@@ -1,26 +1,6 @@
-pub struct Card {
-    denomination: Denomination,
-    suit: Suit
-}
 
-#[derive(PartialEq)]
-enum Denomination {
-    Ace,
-    King,
-    Queen,
-    Jack,
-    Ten,
-    Nine,
-    Eight,
-    Seven,
-    Six,
-    Five,
-    Four,
-    Three,
-    Two
-}
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone)]
 enum Suit {
     Spades,
     Hearts,
@@ -28,8 +8,100 @@ enum Suit {
     Clubs
 }
 
+impl Suit {
+    fn from_char(char: &char) -> Result<Suit, ()> {
+        match char{
+            'S' => Ok(Suit::Spades),
+            'H' => Ok(Suit::Hearts),
+            'D' => Ok(Suit::Diamonds),
+            'C' => Ok(Suit::Clubs),
+            _ => Err(())
+        }
+    }
+}
+
+
+pub struct Card {
+    denomination: Denomination,
+    suit: Option<Suit>
+}
+
+#[derive(PartialEq, Clone)]
+enum Denomination {
+    Ace = 14,
+    King = 13,
+    Queen = 12, 
+    Jack = 11,
+    Ten = 10,
+    Nine = 9,
+    Eight = 8,
+    Seven = 7,
+    Six = 6,
+    Five = 5,
+    Four = 4,
+    Three = 3,
+    Two = 2
+}
+
+
+impl Denomination {
+    fn from_char(char: &char) -> Result<Denomination, ()> {
+        match char {
+            'A' => Ok(Denomination::Ace),
+            'K' => Ok(Denomination::King),
+            'Q' => Ok(Denomination::Queen),
+            'J' => Ok(Denomination::Jack),
+            'T' => Ok(Denomination::Ten),
+            '9' => Ok(Denomination::Nine),
+            '8' => Ok(Denomination::Eight),
+            '7' => Ok(Denomination::Seven),
+            '6' => Ok(Denomination::Six),
+            '5' => Ok(Denomination::Five),
+            '4' => Ok(Denomination::Four),
+            '3' => Ok(Denomination::Three),
+            '2' => Ok(Denomination::Two),
+            _ => Err(())
+        }
+    }
+}
+
+
 impl PartialEq for Card {
     fn eq(&self, other: &Self) -> bool {
-        self.denomination == other.denomination && self.suit == other.suit
+        self.denomination == other.denomination && match (&self.suit, &other.suit) {
+            (Some(s), Some(o)) => s == o,
+            _ => true
+        }
+    }
+}
+
+impl Card {
+    fn from_str(string: &str) -> Result<Card, ()>  {
+        match Denomination::from_char(&string.chars().nth(0).unwrap()) {
+            Ok(d) => Ok(Card { denomination: d, suit: match Suit::from_char(&string.chars().nth(1).unwrap()) {
+                Ok(s) => Some(s),
+                Err(_e) => None
+            }}),
+            Err(e) => Err(e)
+        }
+    }
+}
+
+
+
+impl Eq for Card {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_does_not_work() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
     }
 }
---
+++ tests/integration_tests.rs
@@ -0,0 +1,8 @@
+mod tests {
+    #[test]
+    fn it_works_2() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+}
