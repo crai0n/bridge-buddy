15d00c915f7686edb85a815632c9eab4869df438
crai0n <52631259+crai0n@users.noreply.github.com>
2023-05-08 19:34:56 +0200

    added some from_str methods

--- core/src/bidder.rs
+++ core/src/bidder.rs
@@ -1,24 +1,41 @@
+use crate::error::ParseError;
+use strum::{Display, EnumString};
+
 use itertools::Itertools;
 
 use crate::deal::PlayerPosition;
 
 use crate::card::Suit;
 
-#[derive(Debug, PartialOrd, Ord, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, Display, EnumString, PartialOrd, Ord, PartialEq, Eq, Clone, Copy)]
 pub enum ContractLevel {
+    #[strum(to_string = "1")]
     One,
+    #[strum(to_string = "2")]
     Two,
+    #[strum(to_string = "3")]
     Three,
+    #[strum(to_string = "4")]
     Four,
+    #[strum(to_string = "5")]
     Five,
+    #[strum(to_string = "6")]
     Six,
+    #[strum(to_string = "7")]
     Seven,
 }
 
-#[derive(Debug, PartialOrd, Ord, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, Display, EnumString, PartialOrd, Ord, PartialEq, Eq, Clone, Copy)]
 pub enum ContractState {
+    #[strum(serialize = "p")]
+    #[strum(serialize = "P")]
+    #[strum(to_string = "")]
     Passed,
+    #[strum(serialize = "x")]
+    #[strum(to_string = "X")]
     Doubled,
+    #[strum(serialize = "xx")]
+    #[strum(to_string = "XX")]
     Redoubled,
 }
 
@@ -28,11 +45,112 @@ pub enum ContractDenomination {
     NoTrump,
 }
 
+impl std::fmt::Display for ContractDenomination {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ContractDenomination::Trump(s) => {
+                write!(f, "{}", s)?;
+            }
+            ContractDenomination::NoTrump => {
+                write!(f, "NT")?;
+            }
+        }
+        Ok(())
+    }
+}
+
+impl std::str::FromStr for ContractDenomination {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.chars().count() == 1 {
+            let char = s.chars().next().unwrap();
+            match Suit::from_char(char) {
+                Ok(s) => Ok(ContractDenomination::Trump(s)),
+                Err(e) => Err(e),
+            }
+        } else {
+            match s {
+                "SA" | "NT" => Ok(ContractDenomination::NoTrump),
+                _ => Err(ParseError {
+                    cause: s.into(),
+                    description: "unknown denomination",
+                }),
+            }
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub struct Contract {
     level: ContractLevel,
     denomination: ContractDenomination,
     state: ContractState,
+}
+
+impl std::fmt::Display for Contract {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}{}{}", self.level, self.denomination, self.state)?;
+        Ok(())
+    }
+}
+
+impl std::str::FromStr for Contract {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let len = s.len();
+
+        if len < 2 {
+            return Err(ParseError {
+                cause: s.into(),
+                description: "string too short",
+            });
+        }
+
+        let level = match ContractLevel::from_str(&s[..1]) {
+            Ok(l) => l,
+            Err(_) => {
+                return Err(ParseError {
+                    cause: s.into(),
+                    description: "unknown level",
+                })
+            }
+        };
+
+        let count_doubles = s.chars().rev().take_while(|x| *x == 'x' || *x == 'X').count();
+
+        let state = match count_doubles {
+            0 => ContractState::Passed,
+            1 => ContractState::Doubled,
+            2 => ContractState::Redoubled,
+            _ => {
+                return Err(ParseError {
+                    cause: s.into(),
+                    description: "unknown contract state",
+                })
+            }
+        };
+
+        // rest of the string must be the denomination
+        let den_str = &s[1..len - count_doubles];
+
+        let denomination = match ContractDenomination::from_str(den_str) {
+            Ok(d) => d,
+            Err(_) => {
+                return Err(ParseError {
+                    cause: s.into(),
+                    description: "unknown contract denomination",
+                })
+            }
+        };
+
+        Ok(Contract {
+            level,
+            denomination,
+            state,
+        })
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
@@ -124,12 +242,9 @@ impl BidLine {
             Bid::Auxiliary(AuxiliaryBid::Double) => {
                 // double is possible immediately after a contract bid or after a contract bid and two passes.
                 if let Some(Bid::Contract(_)) = self.bids.last() {
-                    return true;
-                }
-
-                if self.bids.len() >= 3 {
+                    true
+                } else if self.bids.len() >= 3 {
                     let index = self.bids.len() - 3;
-
                     matches!(
                         &self.bids[index..],
                         [
@@ -145,11 +260,9 @@ impl BidLine {
             Bid::Auxiliary(AuxiliaryBid::Redouble) => {
                 // Redouble is possible immediately after a double or after a double and two passes
                 if self.bids.last() == Some(&Bid::Auxiliary(AuxiliaryBid::Double)) {
-                    return true;
-                }
-                if self.bids.len() >= 3 {
+                    true
+                } else if self.bids.len() >= 3 {
                     let index = self.bids.len() - 3;
-
                     matches!(
                         &self.bids[index..],
                         [
@@ -178,7 +291,8 @@ impl BidLine {
 #[cfg(test)]
 mod test {
     use crate::bidder::*;
-    use std::cmp::Ordering;
+    use std::{cmp::Ordering, str::FromStr};
+    use test_case::test_case;
 
     #[test]
     fn contract_ordering_spades_notrump() {
@@ -220,5 +334,38 @@ mod test {
             state,
         };
         assert_eq!(c1.cmp(&c2), Ordering::Less)
+    }
+
+    #[test_case(ContractLevel::One, ContractDenomination::Trump(Suit::Spades), ContractState::Passed, "1♠"; "1P")]
+    #[test_case(ContractLevel::Two, ContractDenomination::Trump(Suit::Hearts), ContractState::Doubled, "2♥X"; "2cx")]
+    #[test_case(ContractLevel::Three, ContractDenomination::NoTrump, ContractState::Redoubled, "3NTXX"; "3ntxx")]
+    fn contract_format(level: ContractLevel, denomination: ContractDenomination, state: ContractState, exp: &str) {
+        assert_eq!(
+            format!(
+                "{}",
+                Contract {
+                    level,
+                    denomination,
+                    state
+                }
+            ),
+            exp
+        );
+    }
+
+    #[test_case("NT", ContractDenomination::NoTrump; "No Trump")]
+    #[test_case("S", ContractDenomination::Trump(Suit::Spades); "Spades")]
+    #[test_case("d", ContractDenomination::Trump(Suit::Diamonds); "Diamonds")]
+    #[test_case("♥", ContractDenomination::Trump(Suit::Hearts); "Hearts")]
+    fn contract_denomination_from_str(str: &str, den: ContractDenomination) {
+        assert_eq!(ContractDenomination::from_str(str).unwrap(), den)
+    }
+
+    #[test_case("1NTx", ContractLevel::One, ContractDenomination::NoTrump, ContractState::Doubled; "No Trump")]
+    #[test_case("2SXx", ContractLevel::Two, ContractDenomination::Trump(Suit::Spades), ContractState::Redoubled; "Spades")]
+    #[test_case("3d", ContractLevel::Three, ContractDenomination::Trump(Suit::Diamonds), ContractState::Passed; "Diamonds")]
+    #[test_case("4♥X",ContractLevel::Four, ContractDenomination::Trump(Suit::Hearts), ContractState::Doubled; "Hearts")]
+    fn contract_from_str(str: &str, level: ContractLevel, denomination: ContractDenomination, state: ContractState) {
+        assert_eq!(Contract::from_str(str).unwrap(), Contract{ level, denomination, state })
     }
 }
