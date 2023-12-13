e5f5b09eb9bc61f9768e4030c944bf1a7a3e4584
crai0n <52631259+crai0n@users.noreply.github.com>
2023-12-13 11:18:51 +0100

    improved coverage

--- core/src/primitives/bid/auxiliary_bid.rs
+++ core/src/primitives/bid/auxiliary_bid.rs
@@ -16,17 +16,6 @@ pub enum AuxiliaryBid {
     Redouble,
 }
 
-// impl std::fmt::Display for AuxiliaryBid {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         match self {
-//             AuxiliaryBid::Pass => write!(f, "Pass")?
-//             AuxiliaryBid::Double => write!(f, "X")?
-//             AuxiliaryBid::Redouble => write!(f, "XX")?
-//         }
-//         Ok(())
-//     }
-// }
-
 #[cfg(test)]
 mod test {
     use super::AuxiliaryBid;
@@ -39,9 +28,13 @@ mod test {
 
     #[test_case("p", Pass; "p is Pass")]
     #[test_case("P", Pass; "P_Pass")]
+    #[test_case("Pass", Pass; "Pass_Pass")]
     #[test_case("X", Double; "x is Double")]
     #[test_case("x", Double; "x_Double")]
     #[test_case("XX", Redouble; "XX_Redouble")]
+    #[test_case("Xx", Redouble; "XX_Redouble1")]
+    #[test_case("xX", Redouble; "XX_Redouble2")]
+    #[test_case("xx", Redouble; "XX_Redouble3")]
     fn from_str(str: &str, bid: AuxiliaryBid) {
         assert_eq!(AuxiliaryBid::from_str(str).unwrap(), bid)
     }
@@ -54,9 +47,19 @@ mod test {
     }
 
     #[test_case(Pass, Pass, Equal; "Pass is equal to Pass")]
+    #[test_case(Double, Double, Equal; "Double is equal to Double")]
+    #[test_case(Redouble, Redouble, Equal; "Redouble is equal to Redouble")]
     #[test_case(Double, Redouble, Less; "Double is less than Redouble")]
     #[test_case(Redouble, Pass, Greater; "Redouble is greater than Pass")]
     fn ordering(one: AuxiliaryBid, other: AuxiliaryBid, expected: Ordering) {
         assert_eq!(one.cmp(&other), expected)
+    }
+
+    #[test]
+    fn is_clone() {
+        let one = AuxiliaryBid::Pass;
+        let two = one;
+
+        assert_eq!(one, two);
     }
 }
