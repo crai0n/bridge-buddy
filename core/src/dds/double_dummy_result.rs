09e7f4d22c2ce451cf819a7273d13fbb5164cd8a
crai0n <52631259+crai0n@users.noreply.github.com>
2024-01-25 14:57:16 +0100

    changed inner organisation of DoubleDummyResult

--- core/src/dds/double_dummy_result.rs
+++ core/src/dds/double_dummy_result.rs
@@ -9,8 +9,11 @@ pub struct DoubleDummyResult {
 }
 
 impl DoubleDummyResult {
-    pub fn max_tricks_for_player_in_strain(&self, player: Seat, strain: Strain) -> usize {
-        let i = match player {
+    pub fn new() -> Self {
+        Self { max_tricks: [0; 20] }
+    }
+    pub fn get_tricks_for_declarer_in_strain(&self, declarer: Seat, strain: Strain) -> usize {
+        let i = match declarer {
             Seat::North => 0,
             Seat::East => 1,
             Seat::South => 2,
@@ -28,6 +31,33 @@ impl DoubleDummyResult {
 
         self.max_tricks[index]
     }
+
+    pub fn set_tricks_for_declarer_in_strain(&mut self, tricks: usize, declarer: Seat, strain: Strain) {
+        let i = match declarer {
+            Seat::North => 0,
+            Seat::East => 1,
+            Seat::South => 2,
+            Seat::West => 3,
+        };
+
+        let j = match strain {
+            Strain::Trump(Suit::Clubs) => 0,
+            Strain::Trump(Suit::Diamonds) => 1,
+            Strain::Trump(Suit::Hearts) => 2,
+            Strain::Trump(Suit::Spades) => 3,
+            Strain::NoTrump => 4,
+        };
+
+        let index = 5 * i + j;
+
+        self.max_tricks[index] = tricks
+    }
+}
+
+impl Default for DoubleDummyResult {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl Display for DoubleDummyResult {
@@ -35,7 +65,7 @@ impl Display for DoubleDummyResult {
         writeln!(f, "  NT ♠S ♥H ♦D ♣C")?;
         for seat in Seat::iter() {
             write!(f, "{} ", seat)?;
-            for strain in 0..5 {
+            for strain in (0..5).rev() {
                 let n_str = format!("{}", self.max_tricks[5 * (seat as usize) + strain]);
                 write!(f, "{:>2} ", n_str)?;
             }
@@ -50,7 +80,7 @@ mod test {
     use crate::dds::double_dummy_result::DoubleDummyResult;
     use test_case::test_case;
 
-    #[test_case([0,1,2,3,4,1,2,3,4,5,2,3,4,5,6,3,4,5,6,7], "  NT ♠S ♥H ♦D ♣C\nN  0  1  2  3  4 \nE  1  2  3  4  5 \nS  2  3  4  5  6 \nW  3  4  5  6  7 \n")]
+    #[test_case([0,1,2,3,4,1,2,3,4,5,2,3,4,5,6,3,4,5,6,7], "  NT ♠S ♥H ♦D ♣C\nN  4  3  2  1  0 \nE  5  4  3  2  1 \nS  6  5  4  3  2 \nW  7  6  5  4  3 \n")]
     fn display(max_tricks: [usize; 20], expected: &str) {
         let ddr = DoubleDummyResult { max_tricks };
         let str = format!("{}", ddr);
--- core/src/dds/mod.rs
+++ core/src/dds/mod.rs
@@ -4,7 +4,6 @@ use crate::primitives::deal::Seat;
 use crate::primitives::{Card, Deal};
 use double_dummy_result::DoubleDummyResult;
 use enum_iterator::all;
-use itertools::Itertools;
 use strum::IntoEnumIterator;
 
 pub mod card_manager;
@@ -16,36 +15,25 @@ mod double_dummy_result;
 pub struct DoubleDummySolver<const N: usize> {}
 
 impl<const N: usize> DoubleDummySolver<N> {
-    const CHECK_QUICK_TRICKS: bool = false;
+    const CHECK_QUICK_TRICKS: bool = true;
 
     pub fn solve(deal: Deal<N>) -> DoubleDummyResult {
         for (seat, hand) in Seat::iter().zip(deal.hands) {
             println!("{}:\n{}", seat, hand)
         }
 
-        let mut result_vec = Vec::new();
+        let mut result = DoubleDummyResult::new();
 
         for declarer in Seat::iter() {
-            let result = Self::solve_for_declarer(deal, declarer);
-            result_vec.extend_from_slice(&result);
+            for strain in all::<Strain>() {
+                let opening_leader = declarer + 1;
+                let defenders_tricks = Self::solve_initial_position(deal, strain, opening_leader);
+                result.set_tricks_for_declarer_in_strain(N - defenders_tricks, declarer, strain);
+            }
         }
 
-        let dds_result = DoubleDummyResult {
-            max_tricks: result_vec.try_into().unwrap(),
-        };
-
-        println!("{}", dds_result);
-        dds_result
-    }
-
-    fn solve_for_declarer(deal: Deal<N>, declarer: Seat) -> [usize; 5] {
-        let mut result = Vec::new();
-        for strain in all::<Strain>().collect_vec().iter().rev() {
-            let opening_leader = declarer + 1;
-            let defenders_tricks = Self::solve_initial_position(deal, *strain, opening_leader);
-            result.push(N - defenders_tricks);
-        }
-        result.try_into().unwrap()
+        println!("{}", result);
+        result
     }
 
     fn solve_initial_position(deal: Deal<N>, strain: Strain, opening_leader: Seat) -> usize {
@@ -53,13 +41,11 @@ impl<const N: usize> DoubleDummySolver<N> {
         let mut at_most = N; // at_most = b - 1;
 
         while at_least < at_most {
-            let target = (at_least + at_most + 1) / 2;
+            let estimate = (at_least + at_most + 1) / 2;
             println!("------------------------");
             println!(
-                "Testing {} tricks for {} as declarer and {:?} as trumps.",
-                target,
-                opening_leader + 3,
-                strain
+                "Trying to make {} tricks for {} as opening leader and {:?}.",
+                estimate, opening_leader, strain
             );
 
             let trumps = match strain {
@@ -69,13 +55,11 @@ impl<const N: usize> DoubleDummySolver<N> {
 
             let mut start_state = DdsRunner::new(deal.hands, opening_leader, trumps);
 
-            let score = Self::score_node(&mut start_state, target);
-
-            if score >= target {
-                // println!("Declarer can reach their target!");
+            let score = Self::score_node(&mut start_state, estimate);
+            println!("Scored {} tricks for defenders", score);
+            if score >= estimate {
                 at_least = score;
             } else {
-                // println!("Defenders can reach their target!");
                 at_most = score;
             }
         }
@@ -83,28 +67,21 @@ impl<const N: usize> DoubleDummySolver<N> {
     }
 
     fn score_node(state: &mut DdsRunner<N>, estimate: usize) -> usize {
-        // println!("Now checking target {} for player {}", target, state.next_to_play());
-        // println!(
-        //     "{:?} has won {} tricks",
-        //     state.next_to_play().axis(),
-        //     state.tricks_won_by_axis(state.next_to_play())
-        // );
-        // println!("There are {} tricks left to play", state.tricks_left());
         if let Some(early_score) = Self::try_early_node_score(state, estimate) {
             return early_score;
         }
 
         if Self::only_one_trick_left_to_play(state) {
-            // println!("Checking last trick!");
             return Self::score_terminal_node(state);
         }
 
         // println!("generating possible moves!");
         let available_moves = Self::generate_moves(state);
-        let mut upper_bound = 0;
+        let mut highest_score = 0;
         for candidate_move in available_moves {
             // println!("trying card {} for {}!", candidate_move, state.next_to_play());
             let current_player = state.next_to_play();
+
             state.play(candidate_move);
             let new_player = state.next_to_play();
             let score = if current_player.same_axis(&new_player) {
@@ -113,13 +90,15 @@ impl<const N: usize> DoubleDummySolver<N> {
                 N - Self::score_node(state, N + 1 - estimate)
             };
             state.undo();
+
             if score >= estimate {
                 return score;
-            } else if score > upper_bound {
-                upper_bound = score
+            } else if score > highest_score {
+                // if we cannot reach estimate, we need the highest score found
+                highest_score = score
             }
         }
-        upper_bound
+        highest_score
     }
 
     fn try_early_node_score(state: &mut DdsRunner<N>, estimate: usize) -> Option<usize> {
@@ -142,18 +121,6 @@ impl<const N: usize> DoubleDummySolver<N> {
             }
         }
         None
-    }
-
-    #[allow(dead_code)]
-    fn have_enough_quick_tricks_for_target(state: &DdsRunner<N>, target: usize) -> bool {
-        if state.player_is_leading() {
-            // println!("We are leading to this trick, so check quick tricks");
-            let total_with_quick_tricks =
-                state.tricks_won_by_axis(state.next_to_play()) + Self::quick_tricks_for_current_player(state) as usize;
-            target <= total_with_quick_tricks
-        } else {
-            false
-        }
     }
 
     fn quick_tricks_for_current_player(state: &DdsRunner<N>) -> u8 {
@@ -221,8 +188,8 @@ mod test {
     use strum::IntoEnumIterator;
     use test_case::test_case;
 
-    #[test_case( 1u64, [0,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0]; "Test A")]
-    #[test_case( 2u64, [0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1]; "Test B")]
+    #[test_case( 1u64, [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0]; "Test A")]
+    #[test_case( 2u64, [0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0]; "Test B")]
     fn solve1(seed: u64, expected: [usize; 20]) {
         let deal: Deal<1> = Deal::from_u64_seed(seed);
 
@@ -235,8 +202,8 @@ mod test {
         assert_eq!(dds_result.max_tricks, expected);
     }
 
-    #[test_case( 1u64, [0,2,2,0,0,0,0,0,2,2,0,2,2,0,0,0,0,0,2,2]; "Test A")]
-    #[test_case( 2u64, [1,1,0,1,2,0,1,2,1,0,0,1,0,0,2,1,1,2,1,0]; "Test B")]
+    #[test_case( 1u64, [0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0]; "Test A")]
+    #[test_case( 2u64, [2, 1, 0, 1, 1, 0, 1, 2, 1, 0, 2, 0, 0, 1, 0, 0, 1, 2, 1, 1]; "Test B")]
     fn solve2(seed: u64, expected: [usize; 20]) {
         let deal: Deal<2> = Deal::from_u64_seed(seed);
 
@@ -250,8 +217,8 @@ mod test {
     }
 
     // #[ignore]
-    #[test_case( 1u64, [0, 0, 0, 1, 0, 2, 1, 2, 1, 2, 1, 2, 0, 1, 0, 2, 1, 2, 1, 2]; "Test A")]
-    #[test_case( 2u64, [2, 0, 3, 3, 3, 0, 3, 0, 1, 0, 2, 0, 3, 2, 3, 0, 3, 0, 0, 0]; "Test B")]
+    #[test_case( 1u64, [0, 1, 0, 0, 0, 2, 1, 2, 1, 2, 0, 1, 0, 2, 1, 2, 1, 2, 1, 2]; "Test A")]
+    #[test_case( 2u64, [3, 3, 3, 0, 2, 0, 1, 0, 3, 0, 3, 2, 3, 0, 2, 0, 0, 0, 3, 0]; "Test B")]
     fn solve3(seed: u64, expected: [usize; 20]) {
         let deal: Deal<3> = Deal::from_u64_seed(seed);
 
@@ -265,10 +232,10 @@ mod test {
         assert_eq!(dds_result.max_tricks, expected);
     }
 
-    #[test_case( 1u64, [3, 2, 4, 3, 4, 2, 2, 1, 2, 1, 3, 2, 4, 3, 4, 2, 2, 1, 2, 1]; "Test A")]
-    #[test_case( 2u64, [0, 2, 1, 0, 3, 4, 3, 4, 5, 2, 1, 2, 1, 0, 3, 4, 3, 4, 5, 2]; "Test B")]
-    #[test_case( 20u64, [0, 0, 3, 0, 3, 3, 4, 1, 4, 0, 0, 0, 4, 0, 4, 3, 5, 1, 4, 1]; "Test C")]
-    #[test_case( 38u64, [1, 3, 4, 1, 0, 1, 2, 1, 3, 4, 0, 3, 4, 1, 0, 1, 2, 1, 3, 4]; "Test D")]
+    #[test_case( 1u64, [4, 3, 4, 2, 3, 1, 2, 1, 2, 2, 4, 3, 4, 2, 3, 1, 2, 1, 2, 2]; "Test A")]
+    #[test_case( 2u64, [3, 0, 1, 2, 0, 2, 5, 4, 3, 4, 3, 0, 1, 2, 1, 2, 5, 4, 3, 4]; "Test B")]
+    #[test_case( 20u64, [3, 0, 3, 0, 0, 0, 4, 1, 4, 3, 4, 0, 4, 0, 0, 1, 4, 1, 5, 3]; "Test C")]
+    #[test_case( 38u64, [0, 1, 4, 3, 1, 4, 3, 1, 2, 1, 0, 1, 4, 3, 0, 4, 3, 1, 2, 1]; "Test D")]
     fn solve5(seed: u64, expected: [usize; 20]) {
         let deal: Deal<5> = Deal::from_u64_seed(seed);
 
@@ -282,9 +249,8 @@ mod test {
         assert_eq!(dds_result.max_tricks, expected);
     }
 
-    #[ignore]
-    #[test_case( 14u64, [5, 4, 5, 1, 2, 1, 2, 0, 4, 4, 2, 3, 6, 1, 2, 1, 2, 0, 4, 4]; "Test A")]
-    #[test_case( 22u64, [3, 6, 6, 1, 1, 2, 0, 0, 4, 4, 3, 6, 6, 1, 1, 2, 0, 0, 4, 5]; "Test B")]
+    #[test_case( 14u64, [2, 1, 5, 4, 5, 4, 4, 0, 2, 1, 2, 1, 6, 3, 2, 4, 4, 0, 2, 1]; "Test A")]
+    #[test_case( 22u64, [1, 1, 6, 6, 3, 4, 4, 0, 0, 2, 1, 1, 6, 6, 3, 5, 4, 0, 0, 2]; "Test B")]
     fn solve6(seed: u64, expected: [usize; 20]) {
         let deal: Deal<6> = Deal::from_u64_seed(seed);
 
@@ -299,8 +265,8 @@ mod test {
     }
 
     #[ignore]
-    #[test_case( 37u64, [1, 1, 1, 2, 4, 6, 7, 7, 5, 4, 1, 1, 1, 2, 4, 6, 7, 7, 5, 4]; "Test A")]
-    #[test_case( 82u64, [5, 4, 5, 8, 6, 2, 3, 2, 0, 2, 5, 4, 5, 8, 6, 2, 3, 2, 0, 2]; "Test B")]
+    #[test_case( 37u64, [4, 2, 1, 1, 1, 4, 5, 7, 7, 6, 4, 2, 1, 1, 1, 4, 5, 7, 7, 6]; "Test A")]
+    #[test_case( 82u64, [6, 8, 5, 4, 5, 2, 0, 2, 3, 2, 6, 8, 5, 4, 5, 2, 0, 2, 3, 2]; "Test B")]
     fn solve8(seed: u64, expected: [usize; 20]) {
         let deal: Deal<8> = Deal::from_u64_seed(seed);
 
@@ -315,8 +281,8 @@ mod test {
     }
 
     #[ignore]
-    #[test_case( 37u64, [2, 2, 4, 1, 4, 7, 6, 4, 8, 5, 2, 2, 4, 1, 4, 7, 6, 4, 8, 5]; "Test A")]
-    #[test_case( 82u64, [5, 4, 5, 8, 6, 2, 3, 2, 0, 2, 5, 4, 5, 8, 6, 2, 3, 2, 0, 2]; "Test B")]
+    #[test_case( 37u64, [1, 0, 2, 3, 0, 8, 9, 7, 6, 9, 1, 0, 2, 3, 0, 7, 9, 5, 6, 8]; "Test A")]
+    #[test_case( 82u64, [4, 1, 4, 2, 2, 5, 8, 4, 6, 7, 4, 1, 4, 2, 2, 5, 8, 4, 6, 7]; "Test B")]
     fn solve9(seed: u64, expected: [usize; 20]) {
         let deal: Deal<9> = Deal::from_u64_seed(seed);
 
@@ -330,7 +296,7 @@ mod test {
         assert_eq!(dds_result.max_tricks, expected);
     }
 
-    #[test_case( "S:A", "H:A", "C:A", "D:A", [0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0]; "Test A")]
+    #[test_case( "S:A", "H:A", "C:A", "D:A", [1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0]; "Test A")]
     fn solve_explicit1(north: &str, east: &str, south: &str, west: &str, expected: [usize; 20]) {
         let north_hand = Hand::<1>::from_str(north).unwrap();
         let east_hand = Hand::<1>::from_str(east).unwrap();
