0871a26886265e0c4852b2121e1f97aeab1dd31c
crai0n <52631259+crai0n@users.noreply.github.com>
2024-01-30 13:58:45 +0100

    extracted MoveGenerator out of DDS

--- dds/src/dds/mod.rs
+++ dds/src/dds/mod.rs
@@ -1,4 +1,4 @@
-use crate::dds::dds_move::DdsMove;
+use crate::dds::move_generator::MoveGenerator;
 use crate::dds::transposition_table::{TTValue, TranspositionTable};
 use crate::dds::virtual_state::VirtualState;
 use bridge_buddy_core::primitives::contract::Strain;
@@ -14,6 +14,7 @@ mod dds_config;
 mod dds_move;
 mod double_dummy_result;
 pub mod double_dummy_state;
+mod move_generator;
 mod transposition_table;
 mod virtual_card;
 pub mod virtual_state;
@@ -108,7 +109,7 @@ impl<const N: usize> DoubleDummySolver<N> {
         }
 
         // println!("generating possible moves!");
-        let available_moves = self.generate_moves(state);
+        let available_moves = MoveGenerator::generate_moves(state, self.config.move_ordering);
         let mut highest_score = 0;
         let mut pv: Vec<Card> = vec![];
         for candidate_move in available_moves {
@@ -172,24 +173,41 @@ impl<const N: usize> DoubleDummySolver<N> {
             // storing in TT doesn't make sense as we can never improve lower bound here
             return Some((current_tricks, state.list_played_cards().to_vec()));
         };
+
         let maximum_tricks = Self::maximum_achievable_tricks(state);
         if maximum_tricks < estimate {
             // storing in TT doesn't make sense as we can never improve upper bound here
             return Some((maximum_tricks, state.list_played_cards().to_vec()));
         };
-        if self.config.check_quick_tricks && state.player_is_leading() {
-            let quick_tricks = Self::quick_tricks_for_current_player(state) as usize;
-            let total_with_quick_tricks = state.tricks_won_by_axis(state.next_to_play()) + quick_tricks;
-            if total_with_quick_tricks >= estimate {
-                if self.config.use_transposition_table && state.player_is_leading() {
-                    let cards = state.list_played_cards().to_vec();
-                    self.store_lower_bound_in_tt(state, quick_tricks, cards);
-                }
-                return Some((total_with_quick_tricks, state.list_played_cards().to_vec()));
+
+        if self.config.use_transposition_table && state.player_is_leading() {
+            if let Some(tt_score) = self.try_find_node_in_tt(state, estimate) {
+                return Some(tt_score);
             }
         }
-        if self.config.use_transposition_table && state.player_is_leading() {
-            return self.try_find_node_in_tt(state, estimate);
+
+        if self.config.check_quick_tricks && state.player_is_leading() {
+            if let Some(qt_score) = self.try_score_using_quick_tricks(state, estimate) {
+                return qt_score;
+            }
+        }
+
+        None
+    }
+
+    fn try_score_using_quick_tricks(
+        &mut self,
+        state: &VirtualState<N>,
+        estimate: usize,
+    ) -> Option<Option<(usize, Vec<Card>)>> {
+        let quick_tricks = Self::quick_tricks_for_current_player(state) as usize;
+        let total_with_quick_tricks = state.tricks_won_by_axis(state.next_to_play()) + quick_tricks;
+        if total_with_quick_tricks >= estimate {
+            if self.config.use_transposition_table {
+                let cards = state.list_played_cards().to_vec();
+                self.store_lower_bound_in_tt(state, quick_tricks, cards);
+            }
+            return Some(Some((total_with_quick_tricks, state.list_played_cards().to_vec())));
         }
         None
     }
@@ -221,31 +239,30 @@ impl<const N: usize> DoubleDummySolver<N> {
 
         Self::play_last_trick(state);
 
-        // println!("{:?} has won the last trick!", state.last_trick_winner());
-
-        let result = state.tricks_won_by_axis(lead);
-        let played_cards = state.list_played_cards().to_vec();
-
+        let score = state.tricks_won_by_axis(lead);
         let winner_of_last_trick = state.last_trick_winner().unwrap();
 
         Self::undo_last_trick(state);
 
         if self.config.use_transposition_table && state.player_is_leading() {
-            // store exact score in tt
-            if winner_of_last_trick.same_axis(&state.next_to_play()) {
-                self.store_lower_bound_in_tt(state, 1, played_cards.clone());
-            } else {
-                self.store_upper_bound_in_tt(state, 0, played_cards.clone());
-            }
+            self.store_terminal_node_in_tt(state, winner_of_last_trick);
         }
 
-        (result, played_cards)
+        (score, state.list_played_cards().to_vec())
+    }
+
+    fn store_terminal_node_in_tt(&mut self, state: &VirtualState<N>, winner_of_last_trick: Seat) {
+        let played_cards = state.list_played_cards().to_vec();
+        if winner_of_last_trick.same_axis(&state.next_to_play()) {
+            self.store_lower_bound_in_tt(state, 1, played_cards);
+        } else {
+            self.store_upper_bound_in_tt(state, 0, played_cards);
+        }
     }
 
     fn play_last_trick(state: &mut VirtualState<N>) {
         for _ in 0..4 {
             let last_card_of_player = state.valid_moves().first().unwrap().card;
-
             state.play(last_card_of_player).unwrap();
         }
     }
@@ -254,95 +271,6 @@ impl<const N: usize> DoubleDummySolver<N> {
         for _ in 0..4 {
             state.undo();
         }
-    }
-
-    fn generate_moves(&self, state: &VirtualState<N>) -> Vec<DdsMove> {
-        let valid_moves = state.valid_moves();
-        let mut unique_moves = Self::select_one_move_per_sequence(&valid_moves);
-        if self.config.move_ordering {
-            self.prioritize_moves(&mut unique_moves, state);
-            Self::sort_moves_by_priority_descending(&mut unique_moves);
-        }
-        unique_moves
-    }
-
-    fn prioritize_moves(&self, moves: &mut [DdsMove], state: &VirtualState<N>) {
-        match state.count_cards_in_current_trick() {
-            0 => Self::prioritize_moves_for_leading_hand(moves, state),
-            1 => Self::prioritize_moves_for_second_hand(moves, state),
-            2 => Self::prioritize_moves_for_third_hand(moves, state),
-            3 => Self::prioritize_moves_for_last_hand(moves, state),
-            _ => unreachable!(),
-        };
-    }
-
-    fn prioritize_moves_for_leading_hand(moves: &mut [DdsMove], state: &VirtualState<N>) {
-        for dds_move in moves {
-            match state.trumps() {
-                None => {
-                    if dds_move.sequence_length >= 3 {
-                        dds_move.priority += 50;
-                    }
-                }
-                Some(trump_suit) => {
-                    if dds_move.sequence_length >= 2 {
-                        dds_move.priority += 50;
-                    }
-
-                    let our_trump_count = state.count_this_sides_trump_cards();
-                    let opponents_trump_count = state.count_opponents_trump_cards();
-
-                    if our_trump_count >= opponents_trump_count && dds_move.card.suit == trump_suit {
-                        dds_move.priority += 100;
-                    }
-                }
-            }
-        }
-    }
-    fn prioritize_moves_for_second_hand(moves: &mut [DdsMove], _state: &VirtualState<N>) {
-        for dds_move in moves {
-            dds_move.priority += 15 - dds_move.card.rank as usize;
-        }
-    }
-    fn prioritize_moves_for_third_hand(moves: &mut [DdsMove], state: &VirtualState<N>) {
-        for dds_move in moves {
-            if dds_move.card > state.currently_winning_card().unwrap() {
-                dds_move.priority += dds_move.card.rank as usize;
-            }
-        }
-    }
-    fn prioritize_moves_for_last_hand(moves: &mut [DdsMove], state: &VirtualState<N>) {
-        for dds_move in moves {
-            if state.current_trick_winner() == state.next_to_play().partner() {
-                dds_move.priority += 15 - dds_move.card.rank as usize;
-            } else {
-                dds_move.priority += dds_move.card.rank as usize;
-            }
-
-            if dds_move.card > state.currently_winning_card().unwrap() {
-                break;
-            }
-        }
-    }
-
-    fn sort_moves_by_priority_descending(moves: &mut [DdsMove]) {
-        moves.sort_unstable_by(|a, b| a.priority.cmp(&b.priority));
-    }
-
-    fn select_one_move_per_sequence(moves: &[DdsMove]) -> Vec<DdsMove> {
-        let mut output: Vec<DdsMove> = vec![];
-        for &candidate_move in moves {
-            if let Some(last) = output.last_mut() {
-                if candidate_move.card.touches(&last.card) {
-                    last.sequence_length += 1;
-                } else {
-                    output.push(candidate_move);
-                }
-            } else {
-                output.push(candidate_move);
-            }
-        }
-        output
     }
 }
 
