3039ca94aa2c574e16136f44fc5f2aebe5ea0e08
crai0n <52631259+crai0n@users.noreply.github.com>
2024-02-14 19:09:42 +0100

    added a DDR bench

--- dds/src/double_dummy_solver/double_dummy_runner.rs
+++ dds/src/double_dummy_solver/double_dummy_runner.rs
@@ -314,3 +314,34 @@ impl DoubleDummyRunner {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    extern crate test;
+
+    use crate::double_dummy_solver::double_dummy_runner::DoubleDummyRunner;
+
+    use bridge_buddy_core::primitives::Deal;
+
+    use bridge_buddy_core::primitives::contract::Strain;
+    use bridge_buddy_core::primitives::deal::Seat;
+    use itertools::Itertools;
+    use test::Bencher;
+
+    #[bench]
+    fn dd_runner(b: &mut Bencher) {
+        const DEAL_SIZE: usize = 4;
+        const N_SOLVES: usize = 10000;
+
+        b.iter(|| {
+            let mut runner = DoubleDummyRunner::default();
+            let deals: [usize; N_SOLVES] = (0..N_SOLVES)
+                .map(|_| Deal::<DEAL_SIZE>::new())
+                .map(|deal| runner.solve_initial_position(deal, Strain::NoTrump, Seat::North))
+                .collect_vec()
+                .try_into()
+                .unwrap();
+            deals
+        })
+    }
+}
