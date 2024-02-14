340a142ee2a407bbc8a1f5355b14653f6d9ed58f
crai0n <52631259+crai0n@users.noreply.github.com>
2024-02-14 20:30:29 +0100

    switched to lazy_static implementation

--- dds/src/state/virtualizer.rs
+++ dds/src/state/virtualizer.rs
@@ -2,12 +2,9 @@ use super::virtual_card::VirtualCard;
 use crate::card_manager::suit_field::SuitField;
 use bridge_buddy_core::primitives::card::virtual_rank::VirtualRank;
 use bridge_buddy_core::primitives::card::Rank;
-use bridge_buddy_core::primitives::{Card, Suit};
+use bridge_buddy_core::primitives::Card;
 use lazy_static::lazy_static;
 use strum::IntoEnumIterator;
-
-include!(concat!(env!("OUT_DIR"), "/virtual_map.rs"));
-include!(concat!(env!("OUT_DIR"), "/absolute_map.rs"));
 
 pub struct Virtualizer {
     played: [SuitField; 4],
@@ -72,14 +69,8 @@ impl Virtualizer {
     pub fn new(played: [SuitField; 4]) -> Self {
         Self { played }
     }
-    pub fn virtual_to_absolute(&self, virtual_card: VirtualCard) -> Option<Card> {
-        let suit = virtual_card.suit;
-        let absolute_rank = self.try_find_absolute(suit, virtual_card.rank);
-        absolute_rank.map(|rank| Card { rank, suit })
-    }
 
-    #[allow(dead_code)]
-    pub fn virtual_to_absolute2(&self, virtual_card: VirtualCard) -> Option<Card> {
+    pub fn virtual_to_absolute(&self, virtual_card: VirtualCard) -> Option<Card> {
         let suit = virtual_card.suit;
         let out_of_play = self.played[suit as usize];
         let absolute_rank = TO_ABSOLUTE_GIVEN_OUT_OF_PLAY[out_of_play.0 as usize][virtual_card.rank as usize];
@@ -88,28 +79,9 @@ impl Virtualizer {
 
     pub fn absolute_to_virtual(&self, card: Card) -> Option<VirtualCard> {
         let suit = card.suit;
-        let virtual_rank = self.try_find_virtual(suit, card.rank);
-        virtual_rank.map(|rank| VirtualCard { rank, suit })
-    }
-
-    #[allow(dead_code)]
-    pub fn absolute_to_virtual2(&self, card: Card) -> Option<VirtualCard> {
-        let suit = card.suit;
         let out_of_play = self.played[suit as usize];
         let virtual_rank = TO_VIRTUAL_GIVEN_OUT_OF_PLAY[out_of_play.0 as usize][card.rank as usize];
         virtual_rank.map(|rank| VirtualCard { rank, suit })
-    }
-
-    fn try_find_absolute(&self, suit: Suit, relative: VirtualRank) -> Option<Rank> {
-        let field = 1u32 << relative as usize;
-        let key = field << 16 | self.played[suit as usize].0 as u32;
-        *ABSOLUTE.get(&key).unwrap()
-    }
-
-    fn try_find_virtual(&self, suit: Suit, absolute: Rank) -> Option<VirtualRank> {
-        let field = 1u32 << absolute as usize;
-        let key = field << 16 | self.played[suit as usize].0 as u32;
-        *VIRTUAL.get(&key).unwrap()
     }
 }
 
@@ -130,20 +102,9 @@ mod test {
         let played = SuitField::from_u16(played);
         let array = [played; 4];
         let virtualizer = Virtualizer::new(array);
-        let relative = virtualizer.try_find_virtual(Suit::Clubs, rank);
-        assert_eq!(relative, expected)
-    }
-
-    #[test_case(Rank::Two, 0b0000_0011_0000_1000, Some(VirtualRank::Five))]
-    #[test_case(Rank::Two, 0b0000_0011_0100_1000, Some(VirtualRank::Six))]
-    #[test_case(Rank::Two, 0b0000_0011_0100_1001, None)]
-    fn virtual_given_played2(rank: Rank, played: u16, expected: Option<VirtualRank>) {
-        let played = SuitField::from_u16(played);
-        let array = [played; 4];
-        let virtualizer = Virtualizer::new(array);
         let suit = Suit::Clubs;
         let expected = expected.map(|rank| VirtualCard { rank, suit });
-        let relative = virtualizer.absolute_to_virtual2(Card { suit, rank });
+        let relative = virtualizer.absolute_to_virtual(Card { suit, rank });
         assert_eq!(relative, expected)
     }
 
@@ -154,20 +115,9 @@ mod test {
         let played = SuitField::from_u16(played);
         let array = [played; 4];
         let virtualizer = Virtualizer::new(array);
-        let absolute = virtualizer.try_find_absolute(Suit::Clubs, rank);
-        assert_eq!(absolute, expected)
-    }
-
-    #[test_case(VirtualRank::Five, 0b0000_0011_0000_1000, Some(Rank::Two))]
-    #[test_case(VirtualRank::Six, 0b0000_0011_0100_1000, Some(Rank::Two))]
-    #[test_case(VirtualRank::Jack, 0b0000_0011_0100_1001, Some(Rank::Nine))]
-    fn absolute_given_played2(rank: VirtualRank, played: u16, expected: Option<Rank>) {
-        let played = SuitField::from_u16(played);
-        let array = [played; 4];
-        let virtualizer = Virtualizer::new(array);
         let suit = Suit::Clubs;
         let expected = expected.map(|rank| Card { rank, suit });
-        let absolute = virtualizer.virtual_to_absolute2(VirtualCard { suit, rank });
+        let absolute = virtualizer.virtual_to_absolute(VirtualCard { suit, rank });
         assert_eq!(absolute, expected)
     }
 }
