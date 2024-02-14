51b2ca71df33c8b3db7c8e8e8ff1727619d1ded2
crai0n <52631259+crai0n@users.noreply.github.com>
2024-02-14 20:28:43 +0100

    fix

--- dds/src/lib.rs
+++ dds/src/lib.rs
@@ -8,4 +8,5 @@ mod transposition_table;
 mod double_dummy_solver;
 mod trick_estimations;
 
+pub use double_dummy_solver::double_dummy_runner::DoubleDummyRunner;
 pub use double_dummy_solver::DoubleDummySolver;
