6979beb2ecb7a90b5347c64754145ec8b4594c8b
crai0n <52631259+crai0n@users.noreply.github.com>
2024-05-26 12:13:11 +0200

    aligned inner and outer for BiddingState

--- core/src/game/game_data/bidding.rs
+++ core/src/game/game_data/bidding.rs
@@ -32,11 +32,76 @@ impl BiddingState {
             board,
         }
     }
+
+    pub fn declarer(&self) -> Option<Seat> {
+        None
+    }
+
+    pub fn hand_of(&self, player: Seat) -> Result<Hand<13>, BBError> {
+        self.hand_manager.hand_of(player)
+    }
+
+    pub fn bidding_has_ended(&self) -> bool {
+        self.bid_manager.bidding_has_ended()
+    }
+
+    pub fn board(&self) -> Board {
+        self.board
+    }
+
+    pub fn validate_make_bid_event(&self, bid_event: BidEvent) -> Result<(), BBError> {
+        self.validate_turn_order(bid_event.player)?;
+        if !self.bid_manager.is_valid_bid(&bid_event.bid) {
+            return Err(BBError::InvalidBid(bid_event.bid));
+        }
+        Ok(())
+    }
+
+    pub fn process_disclose_hand_event(&mut self, event: DiscloseHandEvent) -> Result<(), BBError> {
+        self.hand_manager.register_known_hand(event.hand, event.seat)
+    }
+
+    pub fn process_make_bid_event(&mut self, bid_event: BidEvent) -> Result<(), BBError> {
+        self.validate_make_bid_event(bid_event)?;
+        self.bid_manager.bid(bid_event.bid)?;
+
+        Ok(())
+    }
+
+    pub fn move_to_opening_lead(self, contract: Contract) -> GameData<OpeningLeadState> {
+        let inner = OpeningLeadState {
+            bids: self.bid_manager.bid_line().clone(),
+            trick_manager: TrickManager::new(contract.declarer + 1, contract.trump_suit()),
+            hand_manager: self.hand_manager,
+            contract,
+            board: self.board,
+        };
+
+        GameData { inner }
+    }
+
+    pub fn move_to_ended_without_card_play(self) -> GameData<EndedState> {
+        let inner = EndedState {
+            bids: self.bid_manager.bid_line(),
+            tricks: Vec::new(),
+            hands: self.hand_manager,
+            result: GameResult::Unplayed,
+            board: self.board,
+        };
+
+        GameData { inner }
+    }
 }
 
 impl NextToPlay for GameData<BiddingState> {
     fn next_to_play(&self) -> Seat {
         self.inner.bid_manager.next_to_play()
+    }
+}
+
+impl NextToPlay for BiddingState {
+    fn next_to_play(&self) -> Seat {
+        self.bid_manager.next_to_play()
     }
 }
 
@@ -51,57 +116,34 @@ impl GameData<BiddingState> {
     }
 
     pub fn hand_of(&self, player: Seat) -> Result<Hand<13>, BBError> {
-        self.inner.hand_manager.hand_of(player)
+        self.inner.hand_of(player)
     }
 
     pub fn bidding_has_ended(&self) -> bool {
-        self.inner.bid_manager.bidding_has_ended()
+        self.inner.bidding_has_ended()
     }
 
     pub fn board(&self) -> Board {
-        self.inner.board
+        self.inner.board()
     }
 
     pub fn validate_make_bid_event(&self, bid_event: BidEvent) -> Result<(), BBError> {
-        self.validate_turn_order(bid_event.player)?;
-        if !self.inner.bid_manager.is_valid_bid(&bid_event.bid) {
-            return Err(BBError::InvalidBid(bid_event.bid));
-        }
-        Ok(())
+        self.inner.validate_make_bid_event(bid_event)
     }
 
     pub fn process_disclose_hand_event(&mut self, event: DiscloseHandEvent) -> Result<(), BBError> {
-        self.inner.hand_manager.register_known_hand(event.hand, event.seat)
+        self.inner.process_disclose_hand_event(event)
     }
 
     pub fn process_make_bid_event(&mut self, bid_event: BidEvent) -> Result<(), BBError> {
-        self.validate_make_bid_event(bid_event)?;
-        self.inner.bid_manager.bid(bid_event.bid)?;
-
-        Ok(())
+        self.inner.process_make_bid_event(bid_event)
     }
 
     pub fn move_to_opening_lead(self, contract: Contract) -> GameData<OpeningLeadState> {
-        let inner = OpeningLeadState {
-            bids: self.inner.bid_manager.bid_line().clone(),
-            trick_manager: TrickManager::new(contract.declarer + 1, contract.trump_suit()),
-            hand_manager: self.inner.hand_manager,
-            contract,
-            board: self.inner.board,
-        };
-
-        GameData { inner }
+        self.inner.move_to_opening_lead(contract)
     }
 
     pub fn move_to_ended_without_card_play(self) -> GameData<EndedState> {
-        let inner = EndedState {
-            bids: self.inner.bid_manager.bid_line(),
-            tricks: Vec::new(),
-            hands: self.inner.hand_manager,
-            result: GameResult::Unplayed,
-            board: self.inner.board,
-        };
-
-        GameData { inner }
+        self.inner.move_to_ended_without_card_play()
     }
 }
